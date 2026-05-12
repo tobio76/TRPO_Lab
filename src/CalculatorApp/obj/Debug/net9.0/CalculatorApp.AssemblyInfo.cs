1ad39cb59fced998d88cc9eb4c8d3075b8f66861
tobio76 <tobioakk@gmail.com>
2026-05-12 23:17:06 +0300

    #6 Добавлен метод Divide и тесты

--- src/CalculatorApp/obj/Debug/net9.0/CalculatorApp.AssemblyInfo.cs
+++ src/CalculatorApp/obj/Debug/net9.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b387a689e40d9eda5f05c68bc60b9ad7ead9a110")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
